415f50fd5aa0458a00fe4d924bf4819df0ed381c
mariusz <wrotki@msn.com>
2016-08-30 19:03:00 -0700

    Actore sending docker containers lists somewhere
    But they aren't showing up in websocket client

--- datasource/src/main/scala/datasource/services/PushService.scala
+++ datasource/src/main/scala/datasource/services/PushService.scala
@@ -6,19 +6,18 @@ import java.util
 import java.util.concurrent.TimeUnit
 
 import akka.NotUsed
-import akka.actor.{ActorRef, Props}
+import akka.actor.{ActorSystem, ActorRef, Props}
 import akka.http.scaladsl.model.ws.TextMessage.Strict
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.{Sink, Source}
 import com.spotify.docker.client.messages.Container
-import rx.{Observable, Scheduler}
+import rx.lang.scala.Observable
 import collection.JavaConverters._
 import scala.concurrent.duration._
 
 import spray.json._
-import DefaultJsonProtocol._
 
 import scala.collection.mutable
 import scala.concurrent.forkjoin.ThreadLocalRandom
@@ -33,7 +32,7 @@ case class DockerContainerData(
 
 object PushService extends WebService with DockerJsonProtocol {
 
-
+  implicit val system = ActorSystem()
   override def route: Route =  path("status") {
 
 //    import sys.process._
@@ -42,17 +41,27 @@ object PushService extends WebService with DockerJsonProtocol {
 //
 //    val src = Source(Seq(TextMessage(json)) toList)
 
-    val containers: List[Container] = (DockerService.docker.listContainers() asScala) toList
-    val containersForClient: List[Strict] = containers map { c => TextMessage(DockerContainerData(id=c.id()).toJson.toString) }
-    val src: Source[Strict, NotUsed] = Source(containersForClient)
+//    val containers: List[Container] = (DockerService.docker.listContainers() asScala) toList
+//    val containersForClient: List[Strict] = containers map { c => TextMessage(DockerContainerData(id=c.id()).toJson.toString) }
+//    val src: Source[Strict, NotUsed] = Source(containersForClient)
 
     val containersSource = Source.actorPublisher[Strict](ContainerPublisher.props)
+    val dataPublisherRef = system.actorOf(Props[ContainerPublisher])
 
-    val o = Observable.interval(5L, TimeUnit.SECONDS)
-    o.subscribe(n => println("n = " + n))
+    val o = Observable.interval(5 seconds)
+//    o.subscribe()
+//        o.subscribe( n => println("n:"+ n) )
+        o.subscribe( (n) => {
+            val containers: Seq[Container] = DockerService.docker.listContainers().asScala
+            containers foreach {
+              dataPublisherRef ! _
+            }
+          println("sent")
+        })
 
     extractUpgradeToWebSocket { upgrade =>
       complete(upgrade.handleMessagesWithSinkSource(Sink.ignore, containersSource))
     }
+
   }
 }
